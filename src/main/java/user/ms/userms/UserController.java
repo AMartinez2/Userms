25bc420d13726eef7a414b567abf31dffe9415a8
amartinez2 <austinamartinez2@gmail.com>
2018-11-30 13:55:22 -0800

    Adjusting crossorigins

--- src/main/java/user/ms/userms/UserController.java
+++ src/main/java/user/ms/userms/UserController.java
@@ -11,12 +11,12 @@ import org.springframework.web.bind.annotation.RestController;
 import user.ms.userms.entity.User;
 import user.ms.userms.repository.UserRepository;
 
+@CrossOrigin({"http://localhost:4200/"})
 @RestController
 public class UserController {
     @Autowired
     UserRepository userRepository;
 
-    @CrossOrigin()
     @GetMapping ("/allUsers")
     public List<User> getAll () {
         return userRepository.findAll();
